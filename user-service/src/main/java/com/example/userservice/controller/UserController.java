2997445ca3544d4d223220e1d4029f95774a5a99
dudeoeoeo <dudekekek@naver.com>
2022-08-19 08:43:32 +0900

    user add

---
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,54 @@
+package com.example.userservice.controller;
+
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.service.UserService;
+import com.example.userservice.vo.Greeting;
+import com.example.userservice.vo.RequestUser;
+import com.example.userservice.vo.ResponseUser;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    private UserService userService;
+    private Environment env;
+    private Greeting greeting;
+
+    @Autowired
+    public UserController(Environment env, Greeting greeting, UserService userService) {
+        this.env = env;
+        this.greeting = greeting;
+        this.userService = userService;
+    }
+
+    @GetMapping("/health_check")
+    public String status() {
+        return "It's Working in User Service";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting() {
+//        return env.getProperty("greeting.message");
+        return greeting.getMessage();
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity createUser(@RequestBody RequestUser user) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserDto userDto = mapper.map(user, UserDto.class);
+        userService.createUser(userDto);
+
+        ResponseUser responseUser = mapper.map(userDto, ResponseUser.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
+    }
+}
