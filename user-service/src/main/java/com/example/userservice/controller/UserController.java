118f2697339db0bec5bafe930a61b2866350f153
dudeoeoeo <dudekekek@naver.com>
2022-10-09 12:13:09 +0900

    prometheus

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -6,6 +6,7 @@ import com.example.userservice.service.UserService;
 import com.example.userservice.vo.Greeting;
 import com.example.userservice.vo.RequestUser;
 import com.example.userservice.vo.ResponseUser;
+import io.micrometer.core.annotation.Timed;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,7 @@ import java.util.List;
 @RequestMapping("/")
 public class UserController {
 
+    private Greeting greeting;
     private UserService userService;
     private Environment env;
 
@@ -31,6 +33,7 @@ public class UserController {
     }
 
     @GetMapping("/health_check")
+    @Timed(value = "users.status", longTask = true)
     public String status() {
         return String.format("It's Working in User Service"
                             + ", port(local.server.port) = " +env.getProperty("local.server.port")
@@ -38,6 +41,12 @@ public class UserController {
                             + ", token secret = " +env.getProperty("token.secret")
                             + ", token expiration time = " +env.getProperty("token.expiration_time")
         );
+    }
+
+    @GetMapping("/welcome")
+    @Timed(value = "users.welcome", longTask = true)
+    public String welcome() {
+        return greeting.getMessage();
     }
 
     @GetMapping("/users")
